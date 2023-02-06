7c18c4e61b4f10dc4cb36653f2b4b4348fd5612c
Vivian Chu <mchu65@my.bcit.ca>
2023-02-06 11:19:57 -0800

    Update unit tests for calculate_pay

--- test_calculate_pay.py
+++ test_calculate_pay.py
@@ -4,23 +4,33 @@ from caculate_pay import calculate_pay
 
 class TestCalculatePay(TestCase):
     def test_zeros(self):
-        self.assertEqual(0, calculate_pay(0, 0))
+        self.assertEqual(0, calculate_pay(0, 0))  # testing wages and hours are 0
 
     def test_zero_hour(self):
-        self.assertEqual(0, calculate_pay(0, 5))
+        self.assertEqual(0, calculate_pay(0, 5))  # testing hour is 0
 
     def test_zero_wage(self):
-        self.assertEqual(0, calculate_pay(5, 0))
+        self.assertEqual(0, calculate_pay(5, 0))  # testing wage is 0
+
+    def test_negative(self):
+        self.assertEqual(0, calculate_pay(-1, -1))  # testing hours and wages are 0
 
     def test_negative_hour(self):
-        self.assertEqual(0, calculate_pay(-1, 0))
+        self.assertEqual(0, calculate_pay(-1, 0))  # testing hour is negative
 
     def test_negative_wage(self):
-        self.assertEqual(0, calculate_pay(0, -1))
+        self.assertEqual(0, calculate_pay(0, -1))  # testing hour is negative
 
     def test_regular_wage(self):
-        self.assertEqual(0, calculate_pay(40, 40))
+        self.assertEqual(217.25, calculate_pay(39.5, 5.5))  # testing regular hours
 
     def test_regular_wage(self):
-        self.assertEqual(0, calculate_pay(39.5,5.5 ))
+        self.assertEqual(222, calculate_pay(40, 5.55))  # testing upper bound regular hours
+
+    def test_overtime(self):
+        self.assertEqual(335.5, calculate_pay(50.5, 5.5))  # testing overtime regular hours
+
+
+
+
 
